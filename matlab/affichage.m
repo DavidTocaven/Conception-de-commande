3f96d8518d22cc404f7ce91e21baebcd84d0f1e2
DavidTocaven <David.tocaven@gmail.com>
2017-12-08 12:41:28 +0100

    affichage modele EE0
    pas fini

--- matlab/affichage.m
+++ matlab/affichage.m
@@ -1,7 +1,25 @@
 close all
+%%%%%%%%%%%%%%%%%
+%% simu modèles: 
+%%%%%%%%%%%%%%%%%
+
+%%EE0
+% step
+figure(1)
+step(tf(1),EE0.ee(1))
+title('Réponse temporelle à un échelon de EE0')
+legend('u(t)=1','EE0(t)','Location','southeast')
+%% rampe
+t = tf(1,[1 0]);
+figure(2)
+step(t,t*EE0.ee(1))
+title('Réponse temporelle à une rampe de EE0')
+legend('u(t)=t','EE0(t)','Location','southeast')
+%% bode
 figure(1)
 %subplot(1,2,1)
-bode(EE1.ee,EE2.ee)
+bode(EE0.ee)
+title('Bode de EE0
 legend('EE1','EE2')
 %subplot(1,2,2)
 %bodemag(EE2.ee)
