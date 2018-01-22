58fd490ce98ad0ae6bcfeacd6b99dd4c6c9f2ba0
Lucien <lulu.rakoto@gmail.com>
2018-01-23 00:59:31 +0100

    Calcul Commande Retour d'état integral version Espace d'état

--- matlab/CommandeEE2_RetourIntTD.m
+++ matlab/CommandeEE2_RetourIntTD.m
@@ -3,15 +3,15 @@
 espaceEtat1
 
 %% Cahier des charges
-vp_desire = [-4; -5];
+vp_desire = [-4 ; - 5];
 %% observateur
 obsver.H = EE2.ee.b;
 obsver.M = eye(size(EE2.ee.a));
-obsver.vp = 10*vp_desire; %*3
+obsver.vp =  vp_desire; %*3
 obsver.G = place(EE2.ee.a', EE2.ee.c(2,:)', obsver.vp)'; % Ne pas utiliser acker
-
+%%
 obsver.F = EE2.ee.a - obsver.G*EE2.ee.c(2,:);
-           %   a        b               c       D
+           %   a           b                  c       D
 obsver.ee = ss(obsver.F, [obsver.G obsver.H], obsver.M, 0);
 
 %% commande
@@ -19,7 +19,7 @@ obsver.ee = ss(obsver.F, [obsver.G obsver.H], obsver.M, 0);
 K = place(EE2.ee.a, EE2.ee.b, vp_desire);
 K = [0 K(2)];
 EE2_bf.a = [EE2.ee.a-EE2.ee.b*K     -EE2.ee.b*K;
-            [0 0; 0 0]              obsver.F];
+            [0 0; 0 0]              obsver.F];  
 
 EE2_bf.b = [EE2.ee.b; 0;0];
 
@@ -27,6 +27,7 @@ EE2_bf.c = [EE2.ee.c [0 0;0 0]];
 EE2_bf.ee = ss(EE2_bf.a, EE2_bf.b, EE2_bf.c, EE2.ee.d);
 
 EE2_bf.gain = dcgain(EE2_bf.ee(1));
+%   N = 1/EE2_bf.gain; %Modif En salle 
 %%  Analyse du retour d'etat base observateur 
 % Pour EE1 :
 %Changement de base de EE1
@@ -136,14 +137,68 @@ EE0_bf.c = EE0_obsver.C;
 
 EE0_bf.ee = ss(EE0_bf.a, EE0_bf.b, EE0_bf.c, EE0_obsver.ee.d);
 EE0_bf.vp = eig(EE0_bf.ee);
+EE0_bf.gain = dcgain(EE0_bf.ee(1));
 
-
-%% Retour integral
+N = 1/(EE2_bf.gain);
+%% Retour Intégral : 
+%   Nouvel espace d'état avec x = [x xi] = [theta; omega; (y(t)-yref(t)]
+% 
 EEIntegral.a = [EE2.ee.a [0 ; 0];EE2.ee.c(2,:) 0];
 EEIntegral.b = [EE2.ee.b ; 0];
 EEIntegral.c = [EE2.ee.c [0;0]];
 EEIntegral.d = EE2.ee.d;
-
-EEIntegral.ee = ss(EEIntegral.a, EEIntegral.b, EEIntegral.c, EEIntegral.d);
+% Nouvel gain de placement de pole : K = [Kp Ki]
+% Kp --> poles; Ki --> intégrateur
 EEIntegral.K = place(EEIntegral.a, EEIntegral.b, [3*vp_desire ;-10]);
-EEIntegral.eebf = ss(EEIntegral.a - EEIntegral.b*EEIntegral.K, EEIntegral.b, EEIntegral.c, EEIntegral.d);
+
+
+
+%% Bloc de commande
+Te = 0.052;
+%% input : 
+CommTD.a = [obsver.F-EE2.ee.b*K];
+CommTD.b = [EE2.ee.b*N obsver.G];
+CommTD.c = [-K];
+CommTD.d = [N 0];
+CommTD.ee = ss(CommTD.a, CommTD.b, CommTD.c, CommTD.d);
+CommTD.zz = c2d(CommTD.ee, Te, 'tustin');
+CommTD.ft=tf(CommTD.zz);
+ftRef=CommTD.ft(1);
+ftVs=CommTD.ft(2);
+%%
+ftTD=CommTD.ft(1)+CommTD.ft(2);
+%% test 
+
+%% save in file
+numY =CommTD.ft(1,1).num{1,1};
+numU =CommTD.ft(1,2).num{1,1};
+denumY = CommTD.ft(1,1).den{1,1};
+
+File        = fopen('Parametres_Commande.txt','w+');
+
+fprintf(File,'\t Paramètres de la commande en temps discret \n\n');
+%ny 1 2 3
+fprintf(File,'static float my0 =  %f;\n',numY(1));
+fprintf(File,'static float my1 =  %f;\n',numY(2));
+fprintf(File,'static float my2 =  %f;\n\n',numY(3));
+%d 1 2
+fprintf(File,'static float d1 =  %f;\n',denumY(1));
+fprintf(File,'static float d2 =  %f;\n\n',denumY(2));
+% nu 1 2 3
+fprintf(File,'static float mref0 =  %f;\n',numU(1));
+fprintf(File,'static float mref1 =  %f;\n',numU(2));
+fprintf(File,'static float mref2 =  %f;\n\n',numU(3));
+
+fclose(File);
+
+%% Bloc de commande avec retour d'état intégral
+% Etat = [x_reconstruit ; xi]
+% Sortie = u_commande
+% Entrée = [y_ref ; y]
+CommTD_integral.a = [obsver.F-EE2.ee.b*EEIntegral.K(1:2) EE2.ee.b*EEIntegral.K(3); [0 0 0]];
+CommTD_integral.b = [[0;0] obsver.G ; 1 -1];
+CommTD_integral.c = [-EEIntegral.K(1:2) EEIntegral.K(3)];
+CommTD_integral.d = [0 0];
+CommTD_integral.ee = ss(CommTD_integral.a, CommTD_integral.b, CommTD_integral.c, CommTD_integral.d);
+
+step(CommTD_integral.ee)
