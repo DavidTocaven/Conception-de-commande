adf37f929fdc570f8cbee48b18978c90ba7b866c
DavidTocaven <David.tocaven@gmail.com>
2018-01-25 22:28:46 +0100

    chap6 avance

--- CAN_data_script.m
+++ CAN_data_script.m
@@ -13,8 +13,15 @@ n.canV = 10/2047*(n.can-2048)-5;
 n.cnaV = n.cna * (10/1023) - 5.0;
 %          mes0 = V_can0 * (10.0/1023.0) - 5.0;
 
-plot(-5:5,-5:5,n.canV,n.cnaV)
-legend('CAN idÃ©al','CAN du C167','Location','SouthEast')
+pl=plot(-5:5,-5:5,... %ref
+    n.canV,n.cnaV,... %mesurée
+    n.canV(14),n.cnaV(14),'b+',...%erreur bas
+    n.canV(941),n.cnaV(941),'g+');
+pl(3).LineWidth = 1;
+pl(4).LineWidth = 1;
+legend('Conversions idéales','Conversions du C167','E1','E2','Location','SouthEast')
 axis([-5.5 5.5 -5.5 5.5])
-xlabel('Tension envoyÃ©e (Volt)');
-ylabel('Tension lue (Volt)');
+xlabel(sprintf('Tension équivalente envoyée (Volt)'));
+ylabel('Tension équivalente lue (Volt)');
+title('Test du CAN et du CNA')
+grid on
